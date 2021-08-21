e74836dc2fd74126a34bc9cc4b3629611a31bcf6
Started-learning-2021 <87558797+Started-learning-2021@users.noreply.github.com>
2021-08-21 18:36:55 +1000

    Almost there with the autoemailer
    There are several things I am stuck on: 
    - Appending a header to the initial CSV output which contains the email title and body for each Health Service. This will let the CSV file be automatically used in the Auto-emailer code. 
    - Need to learn to cycle through the rows in the above CSV file so that all emails can be sent using the code, not just the first row. 
    - Need to learn to attach multiple files to each email. The files could be named by Health Service so there is a way to distinguish them in the same directory. Want to send PDFs, Excel Spreadsheets, JPG files. 
    - Finally, I looked around and couldn't figure out how to just draft the email and not auto-send it to the recipient. Maybe that's too hard. 
    -

--- Auto-fill Draft Emails.py
+++ Auto-fill Draft Emails.py
@@ -1,13 +1,44 @@
-from files import readCSV
+import smtplib
+from files import readCSV, writeCSV
+from email.message import EmailMessage
 
 if __name__ == '__main__':
   # Read the rows
-  rows, fieldnames = readCSV('C:\\Users\\Ben\\Desktop\\Python\\Projects\\AutoEmail\\PersonalEmailsVHBA\\RHIF R5 Emails.csv')
+  rows, fieldnames = readCSV('RHIF R5 Emails.csv')
 
-import smtplib
+  for row in rows:
+    recipient = row['Address']
+    subject = row['Title']
+    body = row['Body']
+    print(row)
 
-server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-server.login("pythonemailer02@gmail.com", "Password1234@")
-server.sendmail("pythonemailer02@gmail.com", "healthservice@gmail.com", "email body")
+Email_Address = "pythonemailer02@gmail.com"
+msg = EmailMessage()
+msg['Subject'] = subject
+msg['From'] = Email_Address
+msg['To'] = recipient 
+msg.set_content(body)
 
-server.quit()
+#Want to auto attach say a PDF file, excel spreadsheet and a jpg file.  
+#for file in files: 
+  #with open(file, 'r+') as f: 
+    #file_data = f.read()
+    #file_name = f.name 
+
+#587 is the Gmail server. To encrypt emails using this ID we need to specify below with smtplib: 
+        #smtp.ehlo()
+        #smtp.starttls()
+        #smtp.ehlo()
+
+#Not really sure what I am doing here want like to add several attachments from the same directory for each recipient: 
+#Want to auto attach a PDF file, excel spreadsheet and a jpg file.  
+#msg.add_attachment(file_data, maintype='application', subtype = 'octet-stream', filename = file_name)
+ 
+#It seems the easier way to encrypt is to use the SMTP_SSL and #465 server ID instead of #587. Less code + encryption:
+with smtplib.SMTP_SSL('smtp.gmail.com', '465') as smtp: 
+    smtp.login("pythonemailer02@gmail.com", "Password1234@")
+    smtp.send_message(msg)
+
+    smtp.quit()
+
+#What I haven't done is cycled through the csv file to create say 5 emails at once. 
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import csv
 from files import readCSV, writeCSV
+
+#Create CSV file and Header before looping through the rows of and creating custom emails.
+#The code below creates the correct header, but it is overridden by the below output. 
+
+#header = ['Address', 'Title', 'Body']
+#with open('RHIF R5 Emails.csv', 'a', newline='') as file:
+  #writer = csv.writer(file, delimiter = ',')
+  #writer.writerow([i for i in header])
 
 if __name__ == '__main__':
   # Read the rows
@@ -22,16 +31,20 @@ if __name__ == '__main__':
 
     # Construct the email
     email = f"Dear {row['Health Service Name']} Representative,\n"\
-    "My name is Ben McRae and I am contacting you regarding the RHIF R5 Grant Program you recently accepted.\n"\
-    "The Victorian Health and Building Authority (VHBA) has received your signed letter and I can confirm the following details:\n"\
-    f"{row['Health Service Name']} has been granted {row['Total Grant Values']} for {row['Number of Projects']} project(s) across {row['Number of Locations']} location(s).\n"\
-    "I am employed by a consultancy named Ontoit which has been selected to oversee the RHIF R5 Grant Program.\n"\
-    "As part of Ontoit's engagement my colleague Alex Krombholz and I will establish a kick-off meeting with you.\n"\
-    "At this meeting we will cover the Grant Program remit, milestone date setting, monthly reporting, milestone claims and any specific questions you have. kick-off meetings are programmed to occur between 10 Sept - 10 Oct, 2021.\n"\
-    "To assist me in establishing a meeting, please email back several dates and times within the kick-off period that you are available to attend a one hour meeting. Meetings usually run for 20-30 minutes. However, an hour is suitable for complex projects and questions.\n"\
-    f"We will create a Microsoft Teams meeting based on your availability. Please invite other {row['Health Service Name']} staff and directors you believe will benefit from attending.\n"\
-    "Congratulations on receiving RHIF R5 Grant funding. I look forward to working with you.\n"\
-    "Please contact me or my colleague Alex if you have questions. Our contact details are in the email signatures below.\n"\
+    "\n"\
+    "My name is Ben McRae and I am contacting you regarding the RHIF R5 Grant Program you recently accepted. The Victorian Health and Building Authority (VHBA) has received your signed letter and I can confirm:\n"\
+    f" - {row['Health Service Name']} has been granted {row['Total Grant Values']} for {row['Number of Projects']} project(s) across {row['Number of Locations']} location(s).\n"\
+    "\n"\
+    "I am employed by a consultancy named Ontoit who is selected to oversee the RHIF R5 Grant Program on behalf of VHBA. As part of Ontoit's engagement my colleague Alex Krombholz and I will establish a kick-off meeting with you. This meeting will cover the Grant Program remit, milestone date setting, monthly reporting, milestone claims and any specific questions you have. kick-off meetings are programmed to occur between 10 Sept - 10 Oct, 2021.\n"\
+    "\n"\
+    "Please email back several dates and times within the kick-off period that you are available to attend a meeting for one hour. They usually run for 20-30 minutes. However, an hour is suitable for complex projects and questions.\n"\
+    "\n"\
+    f"I will create a Microsoft Teams meeting based on your availability. Please invite other {row['Health Service Name']} staff and directors you believe will benefit from attending.\n"\
+    "\n"\
+    "Please contact me or my colleague Alex if you have questions. Our details are in the email signatures below.\n"\
+    "\n"\
+    "Congratulations on receiving RHIF R5 Grant funding. We look forward to working with you.\n"\
+    "\n"\
     "Best Regards,\n"\
     "Ben McRae" 
     
